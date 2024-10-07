b57916ab6f1f2fa5a88cfaab5990ea85d62b337e
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-10-06 20:11:20 -0500

    updated file name

--- Backend/Route/questionRoute.js
+++ Backend/Route/questionRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Authentication middleware
-const authMiddleware = require("../Middleware/authMiddleware");
+const authMiddleware = require("../MiddleWare/authMiddleware");
 
 // Question controllers
 const {
