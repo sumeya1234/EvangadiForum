cccb707df093e831ba7b48ed6c126dff88a36295
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-10-15 18:59:02 -0500

    made some changes for deployment

--- Backend/Database/dbConfig.js
+++ Backend/Database/dbConfig.js
@@ -4,7 +4,7 @@ const mysql2 = require('mysql2/promise'); // Use promise-based version
 const dbConnection = mysql2.createPool({
   user: process.env.USER,
   database:process.env.DATABASE ,
-  host: "localhost",
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   connectionLimit: 10
 });
--- Backend/Route/tagRoute.js
+++ Backend/Route/tagRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getTaggedQuestions } = require("../Controller/tagController.js");
 
 // Authentication middleware
-const authMiddleware = require("../Middleware/authMiddleware");
+// const authMiddleware = require("../Middleware/authMiddleware");
 
 // Route to get tagged questions
 router.get("/by-tags", getTaggedQuestions);
